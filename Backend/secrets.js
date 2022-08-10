7618bcea8c23487bc83f7e458220b0536a28b5ce
Mukesh Dani <mukeshdani00@gmail.com>
2022-08-10 11:47:52 +0530

    reset password , send email using nodemailer gmail and connect to UI

---
+++ Backend/secrets.js
@@ -0,0 +1,6 @@
+module.exports = {
+    JWTSECRET: "sdkfshak",
+    DB_LINK: "mongodb+srv://admin:BohYctQDD0twhLoA@cluster0.ufy4c.mongodb.net/?retryWrites=true&w=majority",
+    APP_PASSWORD:"uonjdfyhjdetebmy",
+    APP_EMAIL:"mukeshdanimsit@gmail.com"
+}
