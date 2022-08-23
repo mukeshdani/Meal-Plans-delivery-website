b4e2af08c22250eabbab924225e8e0fab4f5287f
Mukesh Dani <mukeshdani00@gmail.com>
2022-08-23 22:44:54 +0530

    Controller

--- Backend/api.js
+++ Backend/api.js
@@ -7,12 +7,17 @@ const cookieParser = require("cookie-parser");
 // repersent -> collection
 const userRouter = require("./routes/userRoutes");
 const authRouter = require("./routes/authRoutes");
+const planRouter = require("./routes/planRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 // to  add post body data to req.body
 app.use(express.json());
 // add cookies to req.cookies
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/plan", planRouter);
+app.use("/api/v1/review", reviewRouter);
+
 // update user Profile
 // delete user profile
 
